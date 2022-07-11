0c497636fd837061f71a2cd163156bd176bf5512
KingBlackCow <sgs1159@naver.com>
2022-07-12 01:31:50 +0900

    refactor: post 검증로직 리팩토링

--- src/main/java/com/JSlog/JSblog/controller/PostController.java
+++ src/main/java/com/JSlog/JSblog/controller/PostController.java
@@ -25,23 +25,7 @@ public class PostController {
     //GET, POST, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
 
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) {
-        // @RequestParam String title, @RequestParam String content
-        //  Map<String, String> params 이렇게 받아도되고
-        // 데이터를 검증하는 이유
-        // 1. client 개발자가 깜박할 수 있다. 실수로 값을 안보낼 수 있다.
-        // 2. client bug로 값이 누락될 수 있다.
-        if(result.hasErrors()){
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            FieldError firstFieldError = fieldErrors.get(0);
-            String fieldName = firstFieldError.getField(); //title
-            String errorMessage = firstFieldError.getDefaultMessage(); //..에러메시지
-
-            Map<String, String> error = new HashMap<>();
-            error.put(fieldName, errorMessage);
-            return error;
-        }
-        log.info("params={}", params.toString());
+    public Map<String, String> post(@RequestBody @Valid PostCreate params) {
 
         return Map.of();
     }
--- src/test/java/com/JSlog/JSblog/controller/PostControllerTest.java
+++ src/test/java/com/JSlog/JSblog/controller/PostControllerTest.java
@@ -37,8 +37,10 @@ class PostControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\":\"\", \"content\": \"내용입니다.\"}")
                 ) //application/json or x-www-form-urlencodeed
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
+                .andExpect(jsonPath("$.validation.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
     }
 }
