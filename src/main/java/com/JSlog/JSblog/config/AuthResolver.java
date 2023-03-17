d9ad1c5f7f733c85ef1e70eba10eee63fabb164b
KingBlackCow <sgs1159@naver.com>
2023-03-18 01:03:48 +0900

    feat: HandlerMethodArgumentResolver에 JWT parser 추가

--- src/main/java/com/JSlog/JSblog/config/AuthResolver.java
+++ src/main/java/com/JSlog/JSblog/config/AuthResolver.java
@@ -4,10 +4,15 @@ import com.JSlog.JSblog.config.data.UserSession;
 import com.JSlog.JSblog.domain.Session;
 import com.JSlog.JSblog.exception.UnAuthorized;
 import com.JSlog.JSblog.repository.SessionRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
@@ -33,30 +38,40 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
         log.info(">>>{}", appConfig.toString());
 
         // Authorization
-//        String accessToken = webRequest.getHeader("Authorization");
-//        if (accessToken == null || accessToken.equals("")) {
-//            throw new UnAuthorized();
-//        }
+        String jws = webRequest.getHeader("Authorization");
+        if (jws == null || jws.equals("")) {
+            throw new UnAuthorized();
+        }
+
+        try {
+            Jws<Claims> claims = Jwts.parserBuilder()
+                    .setSigningKey(appConfig.getJwtKey())
+                    .build()
+                    .parseClaimsJws(jws);
+            String userId = claims.getBody().getSubject();
+            return new UserSession(Long.parseLong(userId));
+        } catch (JwtException e) {
+            throw new UnAuthorized();
+        }
 
         // Cookie
-        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-        if (servletRequest == null) {
-            log.error("serveletRequest is null");
-            throw new UnAuthorized();
-        }
+//        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+//        if (servletRequest == null) {
+//            log.error("serveletRequest is null");
+//            throw new UnAuthorized();
+//        }
+//
+//        Cookie[] cookies = servletRequest.getCookies();
+//        if (cookies.length == 0) {
+//            log.error("쿠키가 없음");
+//            throw new UnAuthorized();
+//        }
+//
+//        String accessToken = cookies[0].getValue();
+//
+//        Session sessionOption = sessionRepository.findByAccessToken(accessToken)
+//                .orElseThrow(UnAuthorized::new);
 
-        Cookie[] cookies = servletRequest.getCookies();
-        if (cookies.length == 0) {
-            log.error("쿠키가 없음");
-            throw new UnAuthorized();
-        }
-
-        String accessToken = cookies[0].getValue();
-
-        Session sessionOption = sessionRepository.findByAccessToken(accessToken)
-                .orElseThrow(UnAuthorized::new);
-
-
-        return new UserSession(sessionOption.getUser().getId());
+//        return new UserSession(sessionOption.getUser().getId());
     }
 }
