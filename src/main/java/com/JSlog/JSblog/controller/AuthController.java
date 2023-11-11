547d930325320f88baab657716281b4368e8f663
KingBlackCow <sgs1159@naver.com>
2023-11-12 01:18:44 +0900

    feat: security config

--- src/main/java/com/JSlog/JSblog/controller/AuthController.java
+++ src/main/java/com/JSlog/JSblog/controller/AuthController.java
@@ -4,18 +4,22 @@ import com.JSlog.JSblog.request.Signup;
 import com.JSlog.JSblog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/auth/signup")
+    @GetMapping("/login")
+    public String login() {
+        return "로그인 페이지입니다.";
+    }
+
+    @PostMapping("/signup")
     public void signup(@RequestBody Signup signup) {
         authService.signup(signup);
     }
