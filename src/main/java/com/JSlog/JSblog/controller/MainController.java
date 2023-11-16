2f8af7af0c3e507a22583d3cefeea2cb8a3aefd7
KingBlackCow <sgs1159@naver.com>
2023-11-17 01:20:10 +0900

    feat: 로그인 사용자정보 가져오기

--- src/main/java/com/JSlog/JSblog/controller/MainController.java
+++ src/main/java/com/JSlog/JSblog/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.JSlog.JSblog.controller;
 
+import com.JSlog.JSblog.config.UserPrincipal;
 import com.JSlog.JSblog.request.Signup;
 import com.JSlog.JSblog.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,7 +19,8 @@ public class MainController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public String user(@AuthenticationPrincipal UserPrincipal userPrincipal) {
+
         return "사용자 페이지입니다.";
     }
 
